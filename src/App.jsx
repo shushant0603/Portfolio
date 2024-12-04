51b9d3c92c3bd3fd6433fb0e75a603544439caf7
shushant kumar <shushantkumar@192.168.1.7>
2024-12-04 20:24:21 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Home from './Home'
-import Navbar from './Navbar'
+import Navbar from './Navbar.jsx'; 
+import Home from './Home.jsx'; // Ensure these components are correctly imported
+import About from './About.jsx';
+import Projects from './Projects.jsx';
+import CodingProfile from './CodingProfile.jsx';
+import HireMe from './HireMe.jsx';
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <Home/> 
+  
+     <Router>
+     <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/codingProfile" element={<CodingProfile />} />
+        <Route path="/hireMe" element={<HireMe />} />
+      </Routes>
+    </Router>
+      
+     
     </>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-    <div>
-       <header class="header">
-        <div class="logo" id="logo">
-            <a href="#">Portfolio</a>
-            <img src="photos/star.png" alt="" class="star" />
-        </div>
-       
-        {/* <!-- <i class='bx bx-menu' id="menu-icon"></i> --> */}
+  
+      <div>
+        <header className="header">
+          <div className="logo" id="logo">
+            <Link to="/">Portfolio</Link>
+            <img src="photos/star.png" alt="" className="star" />
+          </div>
 
-        <nav class="navbar">
-            <a href="#home" class="active home">Home</a>
-            <a href="#about" class="about">About</a>
-            <a href="#projects" class="projects">Projects</a>
-            <a href="#coding_profile" class="coding_profile">Coding Profiles</a>
-            <a href="#hire-me" class="hire-me"><i class='bx bxs-phone-call'></i>Hire Me</a>
-        </nav>
-        
-    </header>
-    </div>
-  )
+          <nav className="navbar">
+            <Link to="/" className="active home">Home</Link>
+            <Link to="/about" className="about">About</Link>
+            <Link to="/projects" className="projects">Projects</Link>
+            <Link to="/codingProfile" className="coding_profile">Coding Profiles</Link>
+            <Link to="/hireMe" className="hire-me"><i className='bx bxs-phone-call'></i>Hire Me</Link>
+          </nav>
+        </header>
+      </div>
+
+    
+  );
 }
 
-export default Navbar
+export default Navbar;
